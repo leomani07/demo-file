6423d0197d1e01976e6b25473700f0ec6fb5f878
iZat <izat.repo@gmail.com>
2025-09-10 15:11:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$host = "rds.c7a0ge8ccl27.ap-south-1.rds.amazonaws.com";  // RDS endpoint
+$host = "database-1.cliumscw44qs.ap-south-1.rds.amazonaws.com";  // RDS endpoint
 $db_user = "admin";  // DB master username
-$db_pass = "5OvXbfTpoIlClznb6CKk";  // DB password
+$db_pass = "#Secret123.!";  // DB password
 $db_name = "LoginDB";  // DB name
 
 $conn = new mysqli($host, $db_user, $db_pass, $db_name);
